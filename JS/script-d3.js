2ddac57d95485e21fe050d3c70296489e03b2ccc
Oliver Mansfield <O.Mansfield@amaze.com>
2016-08-10 14:17:30 +0100

    Added ball animation code
    Created defs>radialGradient>stop in d3.js. Animates the gradient offset
    to have ball fill itself in. Sweet.

--- JS/script-d3.js
+++ JS/script-d3.js
@@ -1,23 +1,19 @@
-var width = 1000;
-var height = 1000;
-
-var frame = d3.select(".five").append("svg")
-            .attr("width", width)
-            .attr("height", height)
+var frame = d3.select("#five").append("svg")
+            .attr("width", "100%")
+            .attr("height", "100%")
             .attr("class", "svg-frame")
-            .attr("viewBox", "0 0 1000 1000");
+            .attr("viewBox", "0 0 340 500");
 
-//////////////////////////////////////////////////////////////////////////////////
 
   ////////////////
  ///SLASH////////
 ////////////////
-var slashStart = "241.4,0 241.3,0.2 343.1,0.2 343.2,0";
-var slashEnd = "241.4,0 93.2,262.3 195,262.3 343.2,0"; 
+var slashStart = "177.7,20.4 177.5,20.7 244.4,20.7 244.6,20.4";
+var slashEnd = "177.7,20.4 80.4,192.6 147.3,192.6 244.6,20.4"; 
 
 var slash = frame.append("polygon")
 			.attr("points", slashStart)
-			.attr("fill", "#000000");
+			.attr("fill", "#ffdf17");
 
 slash.transition()
 	.duration(500)
@@ -27,14 +23,14 @@ slash.transition()
   ////////////////
  ///LINE/////////
 ////////////////
-var linePath = "M93.2,262l127.3,0c120.6,0,218.3,97.8,218.3,218.3s-97.8,218.3-218.3,218.3C99.9,698.7,2.2,600.9,2.2,480.3	c0-15,1.5-29.6,4.4-43.7"
+var linePath = "M80.6,192.6l83.3,0c79.2,0,143.5,64.2,143.5,143.5s-64.2,143.5-143.5,143.5c-79.2,0-143.5-64.2-143.5-143.5c0-9.8,1-19.4,2.9-28.7"
 var line = frame.append("path")
 			.attr("d", linePath)
 			.attr("fill", "none")
 			.attr("stroke", "#000000")
-			.attr("stroke-width", "2px")
-			.attr("stroke-dasharray", "1200")
-			.attr("stroke-dashoffset", "1200");
+			.attr("stroke-width", "1px")
+			.attr("stroke-dasharray", "830")
+			.attr("stroke-dashoffset", "830");
 
 line.transition()
 	.duration(500)
@@ -44,12 +40,39 @@ line.transition()
   ////////////////
  ///BALL/////////
 ////////////////
-var ball = frame.append("circle")
-			.attr("r", "0")
-			.attr("cx", "392.7")
-			.attr("cy", "65.6")
-			.attr("fill", "#000000");
+var ballGradDefs = frame.append("defs");
+var ballGradRadialGrad = ballGradDefs.append("radialGradient")
+						.attr("id", "ball-grad")
+						.attr("cx", "50%")
+						.attr("cy", "50%")
+						.attr("r", "100%")
+						.attr("fx", "50%")
+						.attr("fy", "50%");
+var ballGradStop1 = ballGradRadialGrad.append("stop")
+					.attr("offset", "49%")
+					.attr("stop-color", "#000000")
+					.attr("stop-opacity", "0");
+var ballGradStop2 = ballGradRadialGrad.append("stop")
+					.attr("offset", "0%")
+					.attr("stop-color", "#000000")
+					.attr("stop-opacity", "1");	
 
-ball.transition()
+var ball = frame.append("circle")
+			.attr("r", "43")
+			.attr("cx", "277.1")
+			.attr("cy", "63.5")
+			.attr("fill","url(#ball-grad)");
+
+ballGradStop1.transition()
 	.duration(500)
-	.attr("r", "65.4");
+	.attr("offset", "30%");
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+var frame = d3.select("#seven").append("svg")
+            .attr("width", "100%")
+            .attr("height", "100%")
+            .attr("class", "svg-frame")
+            .attr("viewBox", "0 0 340 500");
